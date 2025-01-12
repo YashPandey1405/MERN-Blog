f3e25555393aae2fe2af258b5ed06809e0e1fb67
amit712singhal <rakshit.singhal712@gmail.com>
2025-01-12 19:04:35 +0530

    add Firebase integration

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -3,11 +3,12 @@ import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import
-  {
-    signInStart,
-    signInSuccess,
-    signInFailure,
-  } from '../redux/user/userSlice';
+{
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
+import OAuth from '../components/OAuth';
 
 
 export default function SignIn ()
@@ -25,7 +26,8 @@ export default function SignIn ()
     e.preventDefault();
     if ( !formData.email || !formData.password )
     {
-      return dispatch( signInFailure( 'Please fill all the fields' ) );    }
+      return dispatch( signInFailure( 'Please fill all the fields' ) );
+    }
     try
     {
       dispatch( signInStart() );
@@ -51,7 +53,7 @@ export default function SignIn ()
   };
   return (
     <div className='min-h-screen mt-20'>
-      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-6'>
         {/* left */ }
         <div className='flex-1'>
           <Link to='/' className='font-bold dark:text-white text-4xl'>
@@ -101,6 +103,7 @@ export default function SignIn ()
                 'Sign In'
               ) }
             </Button>
+            <OAuth />
           </form>
           <div className='flex gap-2 text-sm mt-5'>
             <span>Don&apos;t have an account?</span>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,3 +1,4 @@
+import OAuth from '../components/OAuth';
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
@@ -46,7 +47,7 @@ export default function SignUp ()
   };
   return (
     <div className='min-h-screen mt-20'>
-      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-6'>
         {/* left */ }
         <div className='flex-1'>
           <Link to='/' className='font-bold dark:text-white text-4xl'>
@@ -105,6 +106,7 @@ export default function SignUp ()
                 'Sign Up'
               ) }
             </Button>
+            <OAuth />
           </form>
           <div className='flex gap-2 text-sm mt-5'>
             <span>Have an account?</span>
